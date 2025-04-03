5729c38eed2f1d8aef3bb0719f96fb25fdad439b
joel <155870458+Joel112003@users.noreply.github.com>
2025-04-04 02:48:55 +0530

    .

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,14 +6,6 @@ import App from "./App";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 // Import all CSS files
-import "./utilis/css/Listing.css";
-import "./utilis/css/ListingDetail.css";
-import "./utilis/css/ListingDetails.css";
-import "./utilis/css/Review.css";
-import "./utilis/css/Map.css";
-import "./utilis/css/Navbar.css";
-import "./utilis/css/CreateListing.css";
-import "./utilis/css/ProfileMenu.css";
 import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
